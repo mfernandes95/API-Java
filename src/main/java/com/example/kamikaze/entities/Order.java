383337e11f5b5d96d798b64a126b07177a4535e4
mpinheiro <mpinheiro@orion.tech>
2020-07-29 18:37:24 -0300

    create entitie order and relation with user

---
+++ src/main/java/com/example/kamikaze/entities/Order.java
@@ -0,0 +1,66 @@
+package com.example.kamikaze.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant date;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order(){}
+
+    public Order(Long id, Instant date, User client) {
+        this.id = id;
+        this.date = date;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+        Order order = (Order) o;
+        return getId().equals(order.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
